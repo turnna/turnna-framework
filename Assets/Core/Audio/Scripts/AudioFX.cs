b362fc57fbea334789c6dccbdbfc73230ee7b1aa
GeorgeCheng <ck40292@gmail.com>
2025-01-10 23:31:47 +0800

    refactor and feat: refactor ui tooltkit to /Resources and add stop event on audioFx

--- Assets/Core/Audio/Scripts/AudioFX.cs
+++ Assets/Core/Audio/Scripts/AudioFX.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class AudioFX : MonoBehaviour
 {
     [Tooltip("Listen to Event Channels")]
-    [SerializeField] private List<VoidEventChannelSO> m_EventChannels;
+    [SerializeField] private List<VoidEventChannelSO> m_StartAudioEventChannels;
+
+    [Tooltip("Listen to Stop Event Channels")]
+    [SerializeField] private List<VoidEventChannelSO> m_StopAudioEventChannels;
 
     private AudioSource m_AudioSource;
 
@@ -15,17 +18,27 @@ public class AudioFX : MonoBehaviour
         m_AudioSource.outputAudioMixerGroup = AudioMixerManager.Instance.SFXAudioGroup;
         m_AudioSource.playOnAwake = false;
 
-        foreach (var eventChannel in m_EventChannels)
+        foreach (var eventChannel in m_StartAudioEventChannels)
         {
             eventChannel.OnEventRaised += PlayAudioFX;
+        }
+
+        foreach (var eventChannel in m_StopAudioEventChannels)
+        {
+            eventChannel.OnEventRaised += StopAudioFX;
         }
     }
 
     public void OnDisable()
     {
-        foreach (var eventChannel in m_EventChannels)
+        foreach (var eventChannel in m_StartAudioEventChannels)
         {
             eventChannel.OnEventRaised -= PlayAudioFX;
+        }
+
+        foreach (var eventChannel in m_StopAudioEventChannels)
+        {
+            eventChannel.OnEventRaised -= StopAudioFX;
         }
     }
 
@@ -33,5 +46,9 @@ public class AudioFX : MonoBehaviour
     {
         m_AudioSource.Play();
     }
-}
 
+    public void StopAudioFX()
+    {
+        m_AudioSource.Stop();
+    }
+}
